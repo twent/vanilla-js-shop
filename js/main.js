7979e0ece22eb93d38c86e1bc34fbf871f7a026f
twent <i@twent.ru>
2023-01-22 00:44:16 +0300

    1️⃣ day - auth modal & simple auth in local storage
    JS code for auth modal & simple auth in local storage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,72 @@
 let authBtn = document.getElementById('open-auth-btn')
+let authModal = document.getElementById('auth-modal')
+let closeModalBtns = authModal.querySelectorAll('.close-btn')
+let loginBtn = authModal.querySelector('.login-btn')
+let logoutBtn = document.getElementById('logout-btn')
+let openCartBtn = document.getElementById('open-cart-btn')
 
-console.log(authBtn)
+let openModal = () => {
+    authModal.classList.add('d-block')
+    
+    setTimeout(() => {
+        authModal.classList.add('show')
+    }, 200)
+}
+
+let closeModal = () => {
+    authModal.classList.remove('show')
+    
+    setTimeout(() => {
+        authModal.classList.remove('d-block')
+    }, 500)
+}
+
+let checkAuth = () => {
+    let authData = JSON.parse(localStorage.getItem('authData'))
+
+    if (authData) {
+        login()
+    }
+}
+
+let login = () => {
+    authBtn.classList.add('d-none')
+
+    logoutBtn.classList.remove('d-none')
+    openCartBtn.classList.remove('d-none')
+    closeModal()
+}
+
+let logout = () => {
+    localStorage.removeItem('authData')
+
+    authBtn.classList.remove('d-none')
+
+    logoutBtn.classList.add('d-none')
+    openCartBtn.classList.add('d-none')
+}
+
+authBtn.addEventListener('click', openModal)
+
+closeModalBtns.forEach((closeModalBtn) => {
+    closeModalBtn.addEventListener('click', closeModal)
+})
+
+loginBtn.addEventListener('click', () => {
+    let loginForm = authModal.querySelector('#login-form')
+    let loginFormData = new FormData(loginForm)
+
+    let authData = {
+        login: loginFormData.get('login'),
+        password: loginFormData.get('password'),
+    }
+
+    localStorage.setItem('authData', JSON.stringify(authData))
+
+    console.log(authData)
+    login()
+})
+
+logoutBtn.addEventListener('click', logout)
+
+checkAuth()
