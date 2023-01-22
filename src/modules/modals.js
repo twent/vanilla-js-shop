56ed833888d54224a466ad8a3e04871c413b5438
twent <i@twent.ru>
2023-01-22 13:31:38 +0300

    2️⃣ day - Add modals backdrop

--- src/modules/modals.js
+++ src/modules/modals.js
@@ -1,18 +1,27 @@
 let closeModalBtns = document.querySelectorAll('.close-btn')
 
 export let openModal = (modal) => {
+    let backdrop = document.createElement('div')
+    backdrop.classList.add('modal-backdrop', 'fade')
+    document.body.append(backdrop)
+
     modal.classList.add('d-block')
     
     setTimeout(() => {
+        backdrop.classList.add('show')
         modal.classList.add('show')
     }, 200)
 }
 
 export let closeModal = (modal) => {
+    let backdrop = document.querySelector('.modal-backdrop')
+
+    backdrop.classList.remove('show')
     modal.classList.remove('show')
     
     setTimeout(() => {
         modal.classList.remove('d-block')
+        backdrop.remove()
     }, 500)
 }
 
